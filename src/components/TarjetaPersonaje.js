da27b4c80624f87e69cef1b355686fb234705763
HelCodex <heleenllp@gmail.com>
2021-12-13 15:37:52 +0100

    n

--- src/components/TarjetaPersonaje.js
+++ src/components/TarjetaPersonaje.js
@@ -6,7 +6,7 @@ export default function TarjetaPersonaje({ name, location }) {
       <img src="#" className="card-img-top" alt="..." />
       <div className="card-body">
         <h5 className="card-title">{name}</h5>
-        <p className="card-location">{location}</p>
+        <p className={("card-location", { location })}>{location}</p>
         <p>Status</p>
       </div>
     </div>
