979132f68882201dfe141eff91db50a02d78808f
Bubbl3Waffl3 <salahlfd0@gmail.com>
2021-12-14 11:26:49 +0100

    changepage function

--- src/components/Characters/Characters.js
+++ src/components/Characters/Characters.js
@@ -3,50 +3,67 @@ import TarjetaPersonaje from '../TarjetaPersonaje';
 
 export default function Characters() {
     let [results, setResults] = useState([]);
+    let [search, setSearch] = useState('');
+    let [status, setStatus] = useState('');
+    let [gender, setGender] = useState('');
+    let [species, setSpecies] = useState('');
+    let [page, setPage] = useState(1);
+    let [maxPages, setMaxPages] = useState()
+    let api = `https://rickandmortyapi.com/api/character?page=${page}&name=${search}&status=${status}&gender=${gender}&species=${species}`;
 
-    let api = "https://rickandmortyapi.com/api/character";
+    function changePage(accion){
+      if (accion === "SUMA" && page < maxPages) {
+        setPage(++page);}
+      if (accion === "RESTA" && page > 1) {
+        setPage(--page);}
+        console.log(page);
+    }
+    function search(){
+      
+    }
 
     useEffect(() => {
         (async () => {
             const response = await fetch(api)
             const data = await response.json()
             setResults(data.results);
+            setMaxPages(data.info.pages)
         })()
     }, [api])
   
     return (
         <React.Fragment>
-        <main className="main">
-        <div>
-          <h2>Characters</h2>
-        </div>
-        <div>
-          <div className="main-filter">
-            <h3>Filters</h3>
+          <main className="main">
+          <div>
+            <h2>Characters</h2>
           </div>
-          <div className="main-listcontainer">
-            <div className="main-listcontainer-searchbar">
-              <input></input>
-              <button>Search</button>
+          <div>
+            <div className="main-filter">
+              <h3>Filters</h3>
             </div>
-            <div className="main-listcontainer-charlist">
-              {results.map((personaje, index) => (
-                <TarjetaPersonaje
-                  key={index}
-                  name={personaje.name}
-                  location={personaje.location.name}
-                  status={personaje.status}
-                  image={personaje.image}
-                ></TarjetaPersonaje>
-              ))}
-            </div>
-            <div>
-              <button></button>
-              <button></button>
+            <div className="main-listcontainer">
+              <div className="main-listcontainer-searchbar">
+                <input></input>
+                <button>Search</button>
+              </div>
+              <div className="main-listcontainer-charlist">
+                {results.map((personaje, index) => (
+                  <TarjetaPersonaje
+                    key={index}
+                    name={personaje.name}
+                    location={personaje.location.name}
+                    status={personaje.status}
+                    image={personaje.image}
+                  ></TarjetaPersonaje>
+                ))}
+              </div>
+              <div>
+                <button onClick={()=> changePage("RESTA")}>anterior</button>
+                <button onClick={()=> changePage("SUMA")}>siguiente</button>
+              </div>
             </div>
           </div>
-        </div>
-      </main>
+        </main>
       </React.Fragment>
     )
 }
