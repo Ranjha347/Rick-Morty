10fba3f741b27c49baf6b22d564eef7e9bcc25fc
HelCodex <heleenllp@gmail.com>
2021-12-15 09:26:42 +0100

    componente filters hecho, pendiente mejorar con for

--- src/components/Characters/Characters.js
+++ src/components/Characters/Characters.js
@@ -1,63 +1,86 @@
-import React, { useState, useEffect } from 'react'
-import TarjetaPersonaje from '../TarjetaPersonaje';
+import React, { useState, useEffect } from "react";
+import TarjetaPersonaje from "../TarjetaPersonaje";
+import Filters from "../Characters/Filters";
 
 export default function Characters() {
-    let [results, setResults] = useState([]);
-    let [search, setSearch] = useState('');
-    let [status, setStatus] = useState('');
-    let [gender, setGender] = useState('');
-    let [species, setSpecies] = useState('');
-    let [page, setPage] = useState(1);
-    let [maxPages, setMaxPages] = useState();
-    let api = `https://rickandmortyapi.com/api/character?page=${page}&name=${search}&status=${status}&gender=${gender}&species=${species}`;
+  let [results, setResults] = useState([]);
+  let [search, setSearch] = useState("");
+  let [status, setStatus] = useState("");
+  let [gender, setGender] = useState("");
+  let [species, setSpecies] = useState("");
+  let [page, setPage] = useState(1);
+  let [maxPages, setMaxPages] = useState();
+  let api = `https://rickandmortyapi.com/api/character?page=${page}&name=${search}&status=${status}&gender=${gender}&species=${species}`;
 
-    function changePage(accion){
-      if (accion === "SUMA" && page < maxPages) {
-        setPage(++page);}
-      if (accion === "RESTA" && page > 1) {
-        setPage(--page);}
-        console.log(page);
+  function changePage(accion) {
+    if (accion === "SUMA" && page < maxPages) {
+      setPage(++page);
     }
-    function changeSearch (event){
-        setSearch(event.target.value) 
-    
+    if (accion === "RESTA" && page > 1) {
+      setPage(--page);
     }
+    console.log(page);
+  }
+  function changeSearch(event) {
+    setSearch(event.target.value);
+  }
 
-    useEffect(() => {
-        (async () => {
-            try{
-            const response = await fetch(api)
-            const data = await response.json()
-            setResults(data.results);
-            setMaxPages(data.info.pages)}
-            catch(error){
-              
-            }}
+  function changeStatus(event) {
+    setStatus(event.target.value);
+    console.log(api);
+  }
 
-        )()
-    }, [api])
-  
-    return (
-        <React.Fragment>
-          <main className="main">
-          <div>
-            <h2>Characters</h2>
+  function changeSpecies(event) {
+    setSpecies(event.target.value);
+    console.log(api);
+  }
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await fetch(api);
+        const data = await response.json();
+        setResults(data.results);
+        setMaxPages(data.info.pages);
+      } catch (error) {}
+    })();
+  }, [api]);
+
+  return (
+    <React.Fragment>
+      <main className="main">
+        <div>
+          <h2>Characters</h2>
+        </div>
+        <div>
+          <div className="main-filter">
+            <h3>Filters</h3>
+            <Filters
+              functional={changeStatus}
+              name={"status"}
+              value1={"alive"}
+              value2={"dead"}
+              value3={"unknow"}
+            ></Filters>
+            <Filters
+              functional={changeSpecies}
+              name={"species"}
+              value1={"human"}
+              value2={"alien"}
+              value3={"humanoid"}
+            ></Filters>
           </div>
-          <div>
-            <div className="main-filter">
-              <h3>Filters</h3>
-            </div>
-            <div className="main-listcontainer">
-              <div className="main-listcontainer-searchbar">
-                <input
+          <div className="main-listcontainer">
+            <div className="main-listcontainer-searchbar">
+              <input
                 type="search"
                 value={search}
                 onChange={changeSearch}
-                ></input>
-                <button>Search</button>
-              </div>
-              <div className="main-listcontainer-charlist">
-                {results ? 
+              ></input>
+              <button>Search</button>
+            </div>
+            <div className="main-listcontainer-charlist">
+              {results ? (
                 results.map((personaje, index) => (
                   <TarjetaPersonaje
                     key={index}
@@ -66,18 +89,18 @@ export default function Characters() {
                     status={personaje.status}
                     image={personaje.image}
                   ></TarjetaPersonaje>
-                )):
-                (
-                  <p>Error ...</p>
-                )}
-              </div>
-              <div>
-                <button onClick={()=> changePage("RESTA")}>anterior</button>
-                <button onClick={()=> changePage("SUMA")}>siguiente</button>
-              </div>
+                ))
+              ) : (
+                <p>Error ...</p>
+              )}
+            </div>
+            <div>
+              <button onClick={() => changePage("RESTA")}>anterior</button>
+              <button onClick={() => changePage("SUMA")}>siguiente</button>
             </div>
           </div>
-        </main>
-      </React.Fragment>
-    )
+        </div>
+      </main>
+    </React.Fragment>
+  );
 }
